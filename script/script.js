8de3abc2f05d8d373f9e67fcb0985673ec5517a2
Lautaro Exequiel Fernández <81919816+Lautarof25@users.noreply.github.com>
2023-08-07 23:18:13 -0300

    refactoring

--- script/script.js
+++ script/script.js
@@ -1,19 +1,10 @@
-// Traigo el body 
 let container = document.querySelector("#container");
-
-// Traigo datos del scrip de español
 let spanish = dataSpanish;
-// Traigo datos del scrip de inglés
 let english = dataEnglish;
-
-// Imprimos por primera vez 
-container.innerHTML += spanish;
-
-// Traigo el botón select
-var select = document.getElementById("select");
-// Creo una variable para guardar 
-// la opción elegida en el select
+let select = document.getElementById("select");
 let selected;
+
+container.innerHTML += spanish;
 
 select.addEventListener("change",(e) =>{
   if (e.target.value == "spanish"){
@@ -27,7 +18,6 @@ select.addEventListener("change",(e) =>{
   }
 })
 
-// Copiar numero de teléfono
 let text = document.getElementById('myText').innerHTML;
 const copyContent = async () => {
   try {
